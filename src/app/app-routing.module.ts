b0afe66422b5a0913fb3b823bc6f9de8ad88d9a9
Settu <vijayan.settu@capgemini.com>
2020-06-06 12:58:42 +0530

    https://github.com/vijayansettu/test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserlistComponent } from './userlist/userlist.component';
 import { UserprofileComponent } from './userprofile/userprofile.component';
-//import { AppComponent } from './app.component';
+
 
 
 const routes: Routes = [
   { path: 'userlist-component', component:  UserlistComponent},
   { path: 'userprofile-component', component: UserprofileComponent },
-  //{ path: '', redirectTo: 'login' },
+  
 ];
 
 @NgModule({
